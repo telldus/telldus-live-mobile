aacd3e50f656d67cdf411e3c878987ad8dd59327
Micke Prag <micke.prag@telldus.se>
2011-08-04 09:43:43 +0200

    Add class TListModel

---
+++ tlistmodel.cpp
@@ -0,0 +1,43 @@
+#include "tlistmodel.h"
+
+#include <QDebug>
+
+class TListModel::PrivateData {
+public:
+	QList<QObject *> list;
+};
+
+TListModel::TListModel(const QByteArray &role, QObject *parent) :
+	QAbstractListModel(parent)
+{
+	d = new PrivateData;
+
+	QHash<int, QByteArray> roles;
+	roles[Qt::UserRole+1] = role;
+	setRoleNames(roles);
+}
+
+TListModel::~TListModel() {
+	delete d;
+}
+
+QVariant TListModel::data(const QModelIndex &index, int role) const {
+	Q_UNUSED(role);
+	return QVariant::fromValue(d->list.at(index.row()));
+}
+
+QVariant TListModel::get(int index) const {
+	return this->data(this->index(index), Qt::DisplayRole);
+}
+
+void TListModel::append(QObject *v) {
+	beginInsertRows( QModelIndex(), d->list.size(), d->list.size() );
+	v->setParent(this);
+	d->list << v;
+	endInsertRows();
+}
+
+int TListModel::rowCount(const QModelIndex &parent) const {
+	Q_UNUSED(parent)
+	return d->list.size();
+}
---
+++ tlistmodel.h
@@ -0,0 +1,31 @@
+#ifndef TLISTMODEL_H
+#define TLISTMODEL_H
+
+#include <QAbstractListModel>
+#include <QScriptValue>
+
+class TListModel : public QAbstractListModel
+{
+	Q_OBJECT
+	Q_PROPERTY(int length READ rowCount)
+public:
+	explicit TListModel(const QByteArray &role = "", QObject *parent = 0);
+	~TListModel();
+
+	virtual int rowCount(const QModelIndex &parent = QModelIndex()) const;
+	virtual QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const;
+
+signals:
+
+public slots:
+	void append(QObject *v);
+	QVariant get(int index) const;
+
+
+private:
+	class PrivateData;
+	PrivateData *d;
+
+};
+
+#endif // TLISTMODEL_H
