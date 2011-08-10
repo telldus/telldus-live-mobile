2c9b4657317bf2e58eccb6c65ae3e201a786ba9e
Micke Prag <micke.prag@telldus.se>
2011-08-10 13:47:09 +0200

    Cache the device type

--- qml/tellduscenterlight/Device.js
+++ qml/tellduscenterlight/Device.js
@@ -42,14 +42,15 @@ function setupCache(deviceModel) {
 
 	db.transaction(function(tx) {
 		//tx.executeSql('DROP TABLE IF EXISTS Device');
-		tx.executeSql('CREATE TABLE IF NOT EXISTS Device(id INTEGER PRIMARY KEY, name TEXT, methods INTEGER, favorite INTEGER, state INTEGER, statevalue TEXT)');
-		var rs = tx.executeSql('SELECT id, name, methods, favorite, state, statevalue FROM Device');
+		tx.executeSql('CREATE TABLE IF NOT EXISTS Device(id INTEGER PRIMARY KEY, name TEXT, methods INTEGER, type INTEGER, favorite INTEGER, state INTEGER, statevalue TEXT)');
+		var rs = tx.executeSql('SELECT id, name, methods, type, favorite, state, statevalue FROM Device');
 		var deviceList = [];
 		for(var i = 0; i < rs.rows.length; ++i) {
 			deviceList.push({
 				'id': rs.rows.item(i).id,
 				'name': rs.rows.item(i).name,
 				'methods': parseInt(rs.rows.item(i).methods, 10),
+				'type': parseInt(rs.rows.item(i).type, 10),
 				'isfavorite': rs.rows.item(i).favorite,
 				'state': parseInt(rs.rows.item(i).state, 10),
 				'statevalue': rs.rows.item(i).statevalue
@@ -62,8 +63,8 @@ function setupCache(deviceModel) {
 function save(devices) {
 	db.transaction(function(tx) {
 		for(var i = 0; i < devices.length; ++i) {
-			tx.executeSql('REPLACE INTO Device (id, name, methods, favorite, state, statevalue) VALUES(?, ?, ?, ?, ?, ?)',
-				[devices[i].id, devices[i].name, devices[i].methods, devices[i].isFavorite, devices[i].state, devices[i].statevalue]
+			tx.executeSql('REPLACE INTO Device (id, name, methods, type, favorite, state, statevalue) VALUES(?, ?, ?, ?, ?, ?, ?)',
+				[devices[i].id, devices[i].name, devices[i].methods, devices[i].type, devices[i].isFavorite, devices[i].state, devices[i].statevalue]
 			);
 		}
 	});
