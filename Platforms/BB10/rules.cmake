151d9156dd0f0786b3239cf12e5c3b2a959ab47d
Micke Prag <micke.prag@telldus.se>
2012-12-14 15:26:58 +0100

    Fix building a .bar file on BB10

--- Platforms/BB10/rules.cmake
+++ Platforms/BB10/rules.cmake
@@ -9,6 +9,12 @@ SET_SOURCE_FILES_PROPERTIES(
 
 LIST(APPEND LIBRARIES -lcpp -lbbsystem )
 
+SET(QNX_TARGET $ENV{QNX_TARGET} CACHE PATH "Path to QNX_TARGET")
+SET(DEBUG_TOKEN "" CACHE FILEPATH "Path to the debug token to sign with")
+SET(DEVICE_PASSWORD "" CACHE FILEPATH "Password to the device")
+SET(DEVICE_IP "169.254.0.1" CACHE STRING "IP address to the device")
+INCLUDE_DIRECTORIES( "${QNX_TARGET}/usr/include" )
+
 FOREACH(file ${BB10_FILES})
 	GET_FILENAME_COMPONENT(filename ${file} NAME)
 	GET_SOURCE_FILE_PROPERTY(path ${file} TARGET_PATH)
@@ -21,9 +27,9 @@ FOREACH(file ${BB10_FILES})
 ENDFOREACH()
 
 FUNCTION(COMPILE target)
-	ADD_CUSTOM_COMMAND(
-		TARGET ${target}
-		POST_BUILD
-		COMMAND blackberry-nativepackager -package ${target}.bar -devMode bar-descriptor.xml ${target}
+	ADD_CUSTOM_TARGET(run
+		blackberry-nativepackager -package ${target}.bar -devMode -debugToken ${DEBUG_TOKEN} -installApp -launchApp -device ${DEVICE_IP} -password ${DEVICE_PASSWORD} bar-descriptor.xml ${target}
+		DEPENDS ${target}
+		COMMENT "Package and deploy .bar file"
 	)
 ENDFUNCTION()
