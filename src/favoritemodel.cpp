700eefae6c9cf8e4c54313fa46cbb929c296437a
Micke Prag <micke.prag@telldus.se>
2012-12-18 11:30:05 +0100

    Add the ability to toggle the filtering on favorites in the favoritesModel

--- src/favoritemodel.cpp
+++ src/favoritemodel.cpp
@@ -4,9 +4,16 @@
 
 #include <QDebug>
 
+class FavoriteModel::PrivateData {
+public:
+	bool doFilter;
+};
+
 FavoriteModel::FavoriteModel(DeviceModel *model, QObject *parent) :
 	QSortFilterProxyModel(parent)
 {
+	d = new PrivateData;
+	d->doFilter = true;
 	this->setSourceModel(model);
 	this->setDynamicSortFilter(true);
 	this->sort(0);
@@ -15,11 +22,18 @@ FavoriteModel::FavoriteModel(DeviceModel *model, QObject *parent) :
 	connect(this, SIGNAL(rowsRemoved(QModelIndex,int,int)), this, SIGNAL(countChanged()));
 }
 
+FavoriteModel::~FavoriteModel() {
+	delete d;
+}
+
 bool FavoriteModel::filterAcceptsRow(int sourceRow, const QModelIndex &) const {
 	DeviceModel *model = qobject_cast<DeviceModel *>(this->sourceModel());
 	if (!model) {
 		//Should not happen
 		return false;
+	}
+	if (!d->doFilter) {
+		return true;
 	}
 	Device *device = qobject_cast<Device *>(model->get(sourceRow).value<QObject *>());
 	return device->isFavorite();
@@ -30,6 +44,16 @@ bool FavoriteModel::lessThan(const QModelIndex &left, const QModelIndex &right) 
 	Device *rightDevice = qobject_cast<Device *>(this->sourceModel()->data(right).value<QObject *>());
 
 	return QString::localeAwareCompare(leftDevice->name(), rightDevice->name()) < 0;
+}
+
+bool FavoriteModel::doFilter() const {
+	return d->doFilter;
+}
+
+void FavoriteModel::setDoFilter(bool doFilter) {
+	d->doFilter = doFilter;
+	emit doFilterChanged();
+	this->invalidateFilter();
 }
 
 void FavoriteModel::rowsAdded(const QModelIndex &, int start, int end) {
--- src/favoritemodel.h
+++ src/favoritemodel.h
@@ -9,11 +9,14 @@ class FavoriteModel : public QSortFilterProxyModel
 {
 	Q_OBJECT
 	Q_PROPERTY(int count READ rowCount NOTIFY countChanged)
+	Q_PROPERTY(bool doFilter READ doFilter WRITE setDoFilter NOTIFY doFilterChanged)
 public:
 	explicit FavoriteModel(DeviceModel *model, QObject *parent = 0);
+	~FavoriteModel();
 
 signals:
 	void countChanged();
+	void doFilterChanged();
 
 protected slots:
 	void rowsAdded( const QModelIndex & parent, int start, int end );
@@ -22,6 +25,12 @@ protected slots:
 protected:
 	bool filterAcceptsRow ( int sourceRow, const QModelIndex & ) const;
 	bool lessThan(const QModelIndex &left, const QModelIndex &right) const;
+
+private:
+	class PrivateData;
+	PrivateData *d;
+	bool doFilter() const;
+	void setDoFilter(bool doFilter);
 
 };
 
