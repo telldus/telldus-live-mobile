3d30f7b6e6f330b4113f8fe5a7fa479a621a8d6d
Micke Prag <micke.prag@telldus.se>
2016-05-24 08:31:20 +0200

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (56 of 56 strings)

--- src/resources/translations/core_sv.ts
+++ src/resources/translations/core_sv.ts
@@ -1,53 +1,53 @@
-<?xml version="1.0" encoding="utf-8"?>
+<?xml version='1.0' encoding='utf-8'?>
 <!DOCTYPE TS>
 <TS version="2.1" language="sv_SE">
 <context>
     <name></name>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="274"/>
-        <location filename="../qml/OverlayDimmer.qml" line="53"/>
-        <location filename="../qml/OverlayDimmer.qml" line="90"/>
-        <location filename="../qml/ComponentSetOnOffTile.qml" line="68"/>
-        <location filename="../qml/ComponentSetOnOffTile.qml" line="170"/>
+        <location filename="../qml/SchedulerPage.qml" line="274"></location>
+        <location filename="../qml/OverlayDimmer.qml" line="53"></location>
+        <location filename="../qml/OverlayDimmer.qml" line="90"></location>
+        <location filename="../qml/ComponentSetOnOffTile.qml" line="68"></location>
+        <location filename="../qml/ComponentSetOnOffTile.qml" line="170"></location>
         <source>On</source>
         <translation>På</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="276"/>
-        <location filename="../qml/OverlayDimmer.qml" line="53"/>
-        <location filename="../qml/OverlayDimmer.qml" line="90"/>
-        <location filename="../qml/ComponentSetOnOffTile.qml" line="125"/>
-        <location filename="../qml/ComponentSetOnOffTile.qml" line="170"/>
+        <location filename="../qml/SchedulerPage.qml" line="276"></location>
+        <location filename="../qml/OverlayDimmer.qml" line="53"></location>
+        <location filename="../qml/OverlayDimmer.qml" line="90"></location>
+        <location filename="../qml/ComponentSetOnOffTile.qml" line="125"></location>
+        <location filename="../qml/ComponentSetOnOffTile.qml" line="170"></location>
         <source>Off</source>
         <translation>Av</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="278"/>
+        <location filename="../qml/SchedulerPage.qml" line="278"></location>
         <source>Bell</source>
         <translation>Ring</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="280"/>
+        <location filename="../qml/SchedulerPage.qml" line="280"></location>
         <source>Dim</source>
         <translation>Dimma</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="282"/>
+        <location filename="../qml/SchedulerPage.qml" line="282"></location>
         <source>Up</source>
         <translation>Upp</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="284"/>
+        <location filename="../qml/SchedulerPage.qml" line="284"></location>
         <source>Down</source>
         <translation>Ned</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="286"/>
+        <location filename="../qml/SchedulerPage.qml" line="286"></location>
         <source>Stop</source>
         <translation>Stopp</translation>
     </message>
     <message>
-        <location filename="../qml/ComponentSetLearn.qml" line="19"/>
+        <location filename="../qml/ComponentSetLearn.qml" line="19"></location>
         <source>Learn</source>
         <translation>Lär upp</translation>
     </message>
@@ -55,87 +55,87 @@
 <context>
     <name>datetime</name>
     <message>
-        <location filename="../qml/SensorPage.qml" line="370"/>
+        <location filename="../qml/SensorPage.qml" line="370"></location>
         <source>Just now</source>
         <translation>Nyss</translation>
     </message>
     <message>
-        <location filename="../qml/SensorPage.qml" line="373"/>
+        <location filename="../qml/SensorPage.qml" line="373"></location>
         <source>1 minute ago</source>
         <translation>1 minut sedan</translation>
     </message>
     <message>
-        <location filename="../qml/SensorPage.qml" line="376"/>
+        <location filename="../qml/SensorPage.qml" line="376"></location>
         <source>minutes ago</source>
         <translation>minuter sedan</translation>
     </message>
     <message>
-        <location filename="../qml/SensorPage.qml" line="380"/>
+        <location filename="../qml/SensorPage.qml" line="380"></location>
         <source>1 hour ago</source>
         <translation>1 timme sedan</translation>
     </message>
     <message>
-        <location filename="../qml/SensorPage.qml" line="383"/>
+        <location filename="../qml/SensorPage.qml" line="383"></location>
         <source>hours ago</source>
         <translation>timmar sedan</translation>
     </message>
     <message>
-        <location filename="../qml/SensorPage.qml" line="387"/>
+        <location filename="../qml/SensorPage.qml" line="387"></location>
         <source>1 day ago</source>
         <translation>1 dag sedan</translation>
     </message>
     <message>
-        <location filename="../qml/SensorPage.qml" line="390"/>
+        <location filename="../qml/SensorPage.qml" line="390"></location>
         <source>days ago</source>
         <translation>dagar sedan</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="244"/>
+        <location filename="../qml/SchedulerPage.qml" line="244"></location>
         <source>Sunday</source>
         <translation>Söndag</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="245"/>
+        <location filename="../qml/SchedulerPage.qml" line="245"></location>
         <source>Monday</source>
         <translation>Måndag</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="246"/>
+        <location filename="../qml/SchedulerPage.qml" line="246"></location>
         <source>Tuesday</source>
         <translation>Tisdag</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="247"/>
+        <location filename="../qml/SchedulerPage.qml" line="247"></location>
         <source>Wednesday</source>
         <translation>Onsdag</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="248"/>
+        <location filename="../qml/SchedulerPage.qml" line="248"></location>
         <source>Thursday</source>
         <translation>Torsdag</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="249"/>
+        <location filename="../qml/SchedulerPage.qml" line="249"></location>
         <source>Friday</source>
         <translation>Fredag</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="250"/>
+        <location filename="../qml/SchedulerPage.qml" line="250"></location>
         <source>Saturday</source>
         <translation>Lördag</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="262"/>
+        <location filename="../qml/SchedulerPage.qml" line="262"></location>
         <source>Today</source>
         <translation>Idag</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="264"/>
+        <location filename="../qml/SchedulerPage.qml" line="264"></location>
         <source>Tomorrow</source>
         <translation>Imorgon</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="266"/>
+        <location filename="../qml/SchedulerPage.qml" line="266"></location>
         <source>Next</source>
         <translation>Nästa</translation>
     </message>
@@ -143,7 +143,7 @@
 <context>
     <name>extra</name>
     <message>
-        <location filename="../qml/TranslationStrings.qml" line="6"/>
+        <location filename="../qml/TranslationStrings.qml" line="6"></location>
         <source>This is untranslated!</source>
         <translation>Detta är översatt till svenska!</translation>
     </message>
@@ -151,25 +151,25 @@
 <context>
     <name>messages</name>
     <message>
-        <location filename="../qml/SettingsPage.qml" line="23"/>
+        <location filename="../qml/SettingsPage.qml" line="23"></location>
         <source>You are using version</source>
         <translation>Du använder version</translation>
     </message>
     <message>
-        <location filename="../qml/SettingsPage.qml" line="23"/>
+        <location filename="../qml/SettingsPage.qml" line="23"></location>
         <source>of</source>
         <translation>av</translation>
     </message>
     <message>
-        <location filename="../qml/SensorPage.qml" line="244"/>
-        <location filename="../qml/SchedulerPage.qml" line="130"/>
+        <location filename="../qml/SensorPage.qml" line="244"></location>
+        <location filename="../qml/SchedulerPage.qml" line="130"></location>
         <source>You can refresh once every 10 seconds.</source>
         <translation>Du kan uppdatera var 10:e sekund.</translation>
     </message>
     <message>
-        <location filename="../qml/SensorPage.qml" line="308"/>
-        <location filename="../qml/SchedulerPage.qml" line="194"/>
-        <location filename="../qml/DevicePage.qml" line="298"/>
+        <location filename="../qml/SensorPage.qml" line="308"></location>
+        <location filename="../qml/SchedulerPage.qml" line="194"></location>
+        <location filename="../qml/DevicePage.qml" line="298"></location>
         <source>Refreshing...
 
 you can only refresh once every 10 seconds!</source>
@@ -178,7 +178,7 @@ you can only refresh once every 10 seconds!</source>
 du kan endast uppdatera var 10:e sekund!</translation>
     </message>
     <message>
-        <location filename="../qml/SensorPage.qml" line="308"/>
+        <location filename="../qml/SensorPage.qml" line="308"></location>
         <source>No sensors have been detected.
 
 Tap here to refresh!</source>
@@ -187,38 +187,38 @@ Tap here to refresh!</source>
 Tryck här för att uppdatera!</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="194"/>
+        <location filename="../qml/SchedulerPage.qml" line="194"></location>
         <source>No schedules have been added yet, please go to http://live.telldus.com to add them. Then tap here to refresh!</source>
         <translation>Inga schemaläggningar har skapats ännu, gå till http://live.telldus.com för att lägga till. Tryck här för att uppdatera!</translation>
     </message>
     <message>
-        <location filename="../qml/PleaseWait.qml" line="11"/>
+        <location filename="../qml/PleaseWait.qml" line="11"></location>
         <source>Please wait...
-...we&apos;ll try to be quick!</source>
+...we'll try to be quick!</source>
         <translation>Vänligen vänta...</translation>
     </message>
     <message>
-        <location filename="../qml/MainMenu.qml" line="77"/>
+        <location filename="../qml/MainMenu.qml" line="77"></location>
         <source>Devices</source>
         <translation>Enheter</translation>
     </message>
     <message>
-        <location filename="../qml/MainMenu.qml" line="114"/>
+        <location filename="../qml/MainMenu.qml" line="114"></location>
         <source>Sensors</source>
         <translation>Sensorer</translation>
     </message>
     <message>
-        <location filename="../qml/LoginWebKitWebView.qml" line="20"/>
+        <location filename="../qml/LoginWebKitWebView.qml" line="20"></location>
         <source>Communicating with the login server</source>
         <translation>Kommunicerar med inloggningsservern</translation>
     </message>
     <message>
-        <location filename="../qml/LoginWebKit.qml" line="70"/>
+        <location filename="../qml/LoginWebKit.qml" line="70"></location>
         <source>To start using Telldus&amp;nbsp;Live!&amp;nbsp;mobile you need to log in to your Telldus&amp;nbsp;Live! account.</source>
         <translation>För att börja använda Telldus&amp;nbsp;Live!&amp;nbsp;mobile behöver du logga in till ditt Telldus&amp;nbsp;Live!-konto.</translation>
     </message>
     <message>
-        <location filename="../qml/DevicePage.qml" line="298"/>
+        <location filename="../qml/DevicePage.qml" line="298"></location>
         <source>No devices have been added yet, please go to http://live.telldus.com to add them.
 
 Then tap here to refresh!</source>
@@ -227,27 +227,27 @@ Then tap here to refresh!</source>
 Tryck sedan här för att uppdatera!</translation>
     </message>
     <message>
-        <location filename="../qml/DeviceDetails.qml" line="35"/>
+        <location filename="../qml/DeviceDetails.qml" line="35"></location>
         <source>Location</source>
         <translation>Plats</translation>
     </message>
     <message>
-        <location filename="../qml/DeviceDetails.qml" line="188"/>
+        <location filename="../qml/DeviceDetails.qml" line="188"></location>
         <source>Device is shown on the dashboard</source>
         <translation>Enheten visas på dashboard</translation>
     </message>
     <message>
-        <location filename="../qml/DeviceDetails.qml" line="188"/>
+        <location filename="../qml/DeviceDetails.qml" line="188"></location>
         <source>Tap to show device on dashboard</source>
         <translation>Tryck för att visa enheten på dashboard</translation>
     </message>
     <message>
-        <location filename="../qml/DeviceDetails.qml" line="194"/>
+        <location filename="../qml/DeviceDetails.qml" line="194"></location>
         <source>Tap to remove</source>
         <translation>Tryck för att ta bort</translation>
     </message>
     <message>
-        <location filename="../qml/DashboardPage.qml" line="161"/>
+        <location filename="../qml/DashboardPage.qml" line="161"></location>
         <source>You have not set any devices or sensors to show in the dashboard, to do this go to their page and click on the star in the top right.</source>
         <translation>Du har inte valt att visa några enheter eller sensorer på dashboard. För att göra det gå till respektive sida och klicka på stjärnan uppe till höger.</translation>
     </message>
@@ -259,71 +259,71 @@ Tryck sedan här för att uppdatera!</translation>
 <context>
     <name>misc</name>
     <message>
-        <location filename="../qml/SettingsPage.qml" line="31"/>
+        <location filename="../qml/SettingsPage.qml" line="31"></location>
         <source>Logout</source>
         <translation>Logga ut</translation>
     </message>
     <message>
-        <location filename="../qml/SensorPage.qml" line="51"/>
+        <location filename="../qml/SensorPage.qml" line="51"></location>
         <source>(no name)</source>
         <translation>(inget namn)</translation>
     </message>
     <message>
-        <location filename="../qml/DevicePage.qml" line="234"/>
-        <location filename="../qml/DashboardPage.qml" line="124"/>
+        <location filename="../qml/DevicePage.qml" line="234"></location>
+        <location filename="../qml/DashboardPage.qml" line="124"></location>
         <source>You can refresh once every 10 seconds.</source>
         <translation>Du kan uppdatera var 10:e sekund.</translation>
     </message>
     <message>
-        <location filename="../qml/ClientDetails.qml" line="23"/>
+        <location filename="../qml/ClientDetails.qml" line="23"></location>
         <source>Include</source>
-        <translation type="unfinished"></translation>
+        <translation>Inkludera</translation>
     </message>
     <message>
-        <location filename="../qml/ClientDetails.qml" line="32"/>
+        <location filename="../qml/ClientDetails.qml" line="32"></location>
         <source>Exclude</source>
-        <translation type="unfinished"></translation>
+        <translation>Exkludera</translation>
     </message>
 </context>
 <context>
     <name>pages</name>
     <message>
-        <location filename="../qml/TranslationStrings.qml" line="8"/>
-        <location filename="../qml/MainMenu.qml" line="40"/>
+        <location filename="../qml/TranslationStrings.qml" line="8"></location>
+        <location filename="../qml/MainMenu.qml" line="40"></location>
         <source>Dashboard</source>
         <translation>Dashboard</translation>
     </message>
     <message>
-        <location filename="../qml/TranslationStrings.qml" line="9"/>
-        <location filename="../qml/DevicePage.qml" line="369"/>
+        <location filename="../qml/TranslationStrings.qml" line="9"></location>
+        <location filename="../qml/DevicePage.qml" line="369"></location>
         <source>Devices</source>
         <translation>Enheter</translation>
     </message>
     <message>
-        <location filename="../qml/TranslationStrings.qml" line="10"/>
-        <location filename="../qml/SensorPage.qml" line="408"/>
+        <location filename="../qml/TranslationStrings.qml" line="10"></location>
+        <location filename="../qml/SensorPage.qml" line="408"></location>
         <source>Sensors</source>
         <translation>Sensorer</translation>
     </message>
     <message>
-        <location filename="../qml/TranslationStrings.qml" line="11"/>
+        <location filename="../qml/TranslationStrings.qml" line="11"></location>
         <source>Scheduler</source>
         <translation>Schemaläggare</translation>
     </message>
     <message>
-        <location filename="../qml/TranslationStrings.qml" line="12"/>
-        <location filename="../qml/SettingsPage.qml" line="42"/>
+        <location filename="../qml/TranslationStrings.qml" line="12"></location>
+        <location filename="../qml/SettingsPage.qml" line="42"></location>
         <source>Settings</source>
         <translation>Inställningar</translation>
     </message>
     <message>
-        <location filename="../qml/TranslationStrings.qml" line="13"/>
-        <location filename="../qml/DebugPage.qml" line="34"/>
+        <location filename="../qml/TranslationStrings.qml" line="13"></location>
+        <location filename="../qml/DebugPage.qml" line="34"></location>
         <source>Debug</source>
         <translation>Debug</translation>
     </message>
     <message>
-        <location filename="../qml/SchedulerPage.qml" line="308"/>
+        <location filename="../qml/SchedulerPage.qml" line="308"></location>
         <source>Upcoming schedule</source>
         <translation>Kommande schema</translation>
     </message>
